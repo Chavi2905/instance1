42dac51660a6cfbbce2e8a20f6772f489a1d5769
Harshit01070143 <harshitsharma16971@gmail.com>
2023-09-05 08:53:55 +0530

    Add project files.

---
+++ instance1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace instance1
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string name, int age, decimal salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.ReadLine();
+            Console.WriteLine($"Age: {Age}");
+            Console.ReadLine(); 
+            Console.WriteLine($"Salary: ${Salary:F2}");
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee1 = new Employee("John Doe", 30, 50000.00M);
+
+           
+            employee1.DisplayDetails();
+        }
+    }
+    
+
+
+
+
+
+}
